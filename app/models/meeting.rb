01900e814f817ee50f7e10c6a2037fae3110bc76
Ekta Verma <eku4evr@gmail.com>
2012-02-01 17:17:33 +0530

    Modified model/meetings

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,8 +2,8 @@ class Meeting < ActiveRecord::Base
   
   belongs_to :meeting_type
   has_many :attendees
-#  serialize :attendees_ids
+  #serialize :attendees_ids
   
-  #  validates :name , :presence => true
-  #  validates :description , :presence => true
+  validates :name , :presence => true
+  #validates :description , :presence => true
 end
